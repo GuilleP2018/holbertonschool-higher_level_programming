93e7d6fce80ca8bbdbcddef8d8546c61114369d1
Guillermo Pereyo <gpereyo@gmail.com>
2023-09-25 18:49:33 -0400

    try 2

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 ##!/usr/bin/python3
 def islower(c):
-    number = ord(c)
-    if number >= 97 and number <= 122:
-        return(1)
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
     else:
-        return(0)
+        return False
