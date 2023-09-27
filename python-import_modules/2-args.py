4e35ccf1660c50c9607cd559ea13cbfc146c599c
Guillermo Pereyo <gpereyo@gmail.com>
2023-09-27 13:49:42 -0400

    grammar fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
 argc = len(sys.argv)
 
 if argc == 1:
-    print("0 Arguments.")
+    print("0 arguments.")
 elif argc == 2:
-    print("1 Argument:")
+    print("1 argument:")
     print("1: {}".format(str(sys.argv[1])))
 else:
-    print("{} Arguments:".format(argc - 1))
+    print("{} arguments:".format(argc - 1))
     for i in range(1, argc):
         print("{}: {}".format(i, str(sys.argv[i])))
