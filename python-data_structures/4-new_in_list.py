7a017184051a326b18d18bcd9aae1b972f507c2d
Guillermo Pereyo <gpereyo@gmail.com>
2023-10-02 17:57:16 -0400

    created a temp list to not overwrit the original

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    temp_list = my_list
     if idx < 0:
         return my_list
     elif idx > len(my_list) - 1:
         return my_list
     else:
-        my_list[idx] = element
-        return my_list
+        temp_list[idx] = element
+        return temp_list
