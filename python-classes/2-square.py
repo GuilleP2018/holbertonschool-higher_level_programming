2b3bbf71710d487d6784679e5d716bddd322cc1a
GuilleP2018 <gpereyo@gmail.com>
2023-10-10 10:18:04 -0400

    update 2 squares

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""This module contains a square class"""
+
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size=0):
+        """initializes the data"""
+        if type(size) is not int:
+            raise TypeError("size is ")
