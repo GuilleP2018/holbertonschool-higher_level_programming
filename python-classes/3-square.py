f307496d4b92446583101bc13691ef37151a3b6d
GuilleP2018 <gpereyo@gmail.com>
2023-10-10 10:31:42 -0400

    3 square and 4 square updates

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,14 +6,14 @@ class Square:
     """This class defines a square"""
 
     def __init__(self, size=0):
-        """initializes the data"""
-        if type(size) is not int:
+        """initializes th9e data"""
+        if not isinstance(size, int):
             raise TypeError("size must be an int")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__Square__size = size
+            self.__size = size
 
     def area(self):
         """generates the area"""
-        return self.__Square__size ** 2
+        return self.__size ** 2
