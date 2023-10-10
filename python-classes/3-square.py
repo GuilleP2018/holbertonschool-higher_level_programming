78012d5269ccf63149f0088310da6a000f9138b7
GuilleP2018 <gpereyo@gmail.com>
2023-10-10 10:22:39 -0400

    Update for two and 3 squares pushed

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""This module contains a square class"""
+
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size=0):
+        """initializes the data"""
+        if type(size) is not int:
+            raise TypeError("size must be an int")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__Square__size = size
+
+    def area(self):
+        """generates the area"""
+        return self.__Square__size ** 2
