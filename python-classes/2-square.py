9fc82f7bde71249512ef0751f766a016c611b5cd
GuilleP2018 <gpereyo@gmail.com>
2023-10-10 10:25:54 -0400

    2 square update

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,9 +7,9 @@ class Square:
 
     def __init__(self, size=0):
         """initializes the data"""
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an int")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self._Square__size = size
+            self.__size = size
