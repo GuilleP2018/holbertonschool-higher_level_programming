0879c1ac366d1f9ec24df41fb1ec84ffd3de8505
GuilleP2018 <gpereyo@gmail.com>
2023-10-13 11:10:01 -0400

    progress and fixes

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,7 +3,15 @@
 
 
 def matrix_divided(matrix, div):
-    """matrix divided function"""
+    """>>> matrix_divided([[]], 3)
+[[]]
+>>> matrix_divided([[1, 2, 3], [4, 5, 6]], 3)
+[[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
+>>> matrix_divided([[1, 2, 3], [4, 5, 6]], -2)
+[[-0.5, -1.0, -1.5], [-2.0, -2.5, -3.0]]
+>>> matrix_divided([[1.5, 2.8, 3.3], [4.2, 5.5, 6.6]], 2)
+[[0.75, 1.4, 1.65], [2.1, 2.75, 3.3]]
+>>> matrix_divided([[1, 2, 3], [4, 5, 6]], 0)"""
     if type(matrix) is not list:
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
