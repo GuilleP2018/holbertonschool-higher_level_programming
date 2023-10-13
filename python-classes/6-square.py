44e1ad55cae76026b86847928beed944534f39ad
GuilleP2018 <gpereyo@gmail.com>
2023-10-13 11:10:13 -0400

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -46,10 +46,11 @@ class Square:
     def my_print(self):
         """Print square"""
         if self.__size == 0:
-            print()
-        else:
-            for x in range(self.__position[1]):
-                print()
-            for z in range(self.__size):
-                print(" " * self.__position[0], end="")
-                print("#" * self.__size)
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
