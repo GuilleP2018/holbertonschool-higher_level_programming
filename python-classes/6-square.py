d664552773173260289f49552628015059e576b3
GuilleP2018 <gpereyo@gmail.com>
2023-10-13 22:48:45 -0400

    fix for 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,8 +7,18 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """initializes the data"""
-        self.__size = size
-        self.__position = position
+        if (not isinstance(position, tuple) or
+                len(position) != 2 or
+                not all(isinstance(num, int) for num in position) or
+                not all(num >= 0 for num in position)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            self.__position = position
 
     @property
     def size(self):
