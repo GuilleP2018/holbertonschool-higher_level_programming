35872a2c0c0fdcdff9445d0c77c96df1bf320f59
GuilleP2018 <gpereyo@gmail.com>
2023-10-19 11:25:00 -0400

    didnt work with issubclass

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """checks if obj is a from a subclass of a_class"""
-    if issubclass(obj, a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
