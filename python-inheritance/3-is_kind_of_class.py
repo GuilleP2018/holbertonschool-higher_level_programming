ea787a2b6a880fcfd605e9dc6de250c9e7bc1701
GuilleP2018 <gpereyo@gmail.com>
2023-10-19 09:40:33 -0400

    first upload is kind of class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module that checks if its from the same class or inherits from it"""
+
+
+def is_kind_of_class(obj, a_class):
+    """true if same class or inherits, false otherwise"""
+    if isinstance(obj, a_class):
+        return True
+    elif issubclass(obj, a_class):
+        return True
+    else:
+        return False
