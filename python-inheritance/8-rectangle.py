61363d2e3795835b42fa9427ad8e68b1e2542d4c
GuilleP2018 <gpereyo@gmail.com>
2023-10-19 12:15:53 -0400

    8 rectangle first upload

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Create class that inherits from basegeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class rectangle inheriting basegeometry"""
+
+    def __init__(self, width, height):
+        """instatiation of width and height"""
+        self.__width = width
+        self.__height = height
