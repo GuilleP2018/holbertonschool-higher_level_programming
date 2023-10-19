087bf9147a60f13ef69bf407a20f12f9e46d3c43
GuilleP2018 <gpereyo@gmail.com>
2023-10-19 12:32:36 -0400

    update for 3, getting full checks

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,7 @@
 
 def is_kind_of_class(obj, a_class):
     """true if same class or inherits, false otherwise"""
-    if isinstance(obj, a_class):
+    if type(obj) is a_class or isinstance(obj, a_class):
         return True
     else:
         return False
