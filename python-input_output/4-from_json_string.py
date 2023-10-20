d96997ec3ce8ff237aa985e98fe581971c39ac7e
GuilleP2018 <gpereyo@gmail.com>
2023-10-20 13:27:06 -0400

    4-from_json_string upload

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for load_from_json_file method."""
+
+import json
+
+
+def from_json_file(my_str):
+    """Method for load_from_json_file."""
+    return json.load(my_str)
