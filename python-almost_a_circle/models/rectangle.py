c131e5de433b885c040376e9fc8f09c5cca17452
GuilleP2018 <gpereyo@gmail.com>
2023-10-24 13:58:21 -0400

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,49 @@ class Rectangle(Base):
         for row in range(self.height):
             print(" "*self.x, end="")
             print("#"*self.width)
+
+        def __str__(self):
+            """override __str__ with new string in the format
+            [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+            str_rep = "[Rectangle] ({}) {}/{} - {}/{}".format(
+                str(self.id), str(self.x), str(self.y),
+                str(self.width), str(self.height))
+            return (str_rep)
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute of Rectangle"""
+        attr_list = ["id", "width", "height", "x", "y"]
+        if args and len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                if i == 1:
+                    self.width = args[i]
+                if i == 2:
+                    self.height = args[i]
+                if i == 3:
+                    self.x = args[i]
+                if i == 4:
+                    self.y = args[i]
+        else:
+            for kw in kwargs:
+                if kw == "id":
+                    self.id = (kwargs[kw])
+                if kw == "width":
+                    self.width = (kwargs[kw])
+                if kw == "height":
+                    self.height = (kwargs[kw])
+                if kw == "x":
+                    self.x = (kwargs[kw])
+                if kw == "y":
+                    self.y = (kwargs[kw])
+
+    def to_dictionary(self):
+        """returns dictionary representation of a Rectangle"""
+        dict_rep = {}
+        dict_rep["id"] = self.id
+        dict_rep["width"] = self.width
+        dict_rep["height"] = self.height
+        dict_rep["x"] = self.x
+        dict_rep["y"] = self.y
+        return dict_rep
