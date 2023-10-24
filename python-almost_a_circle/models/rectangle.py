6c4d9363f3ef71dd1636202125e4652ebcd38c6a
GuilleP2018 <gpereyo@gmail.com>
2023-10-24 10:44:47 -0400

    getters and setters with errors raised

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -13,9 +13,25 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Init function"""
         super().__init__(id)
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        elif x <= 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        elif y <= 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
 
     @property
@@ -41,19 +57,35 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Width setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
         """height setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @x.setter
     def x(self, value):
         """x setter"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value <= 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
         """y setter"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value <= 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
