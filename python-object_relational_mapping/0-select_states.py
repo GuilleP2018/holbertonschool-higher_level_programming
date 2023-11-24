a5e5a2e7159f0a49ec3aec84c750b652f94a7e6c
GuilleP2018 <gpereyo@gmail.com>
2023-11-24 15:06:26 -0400

    Permisions

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,13 +3,16 @@
 Module for connecting to the database hbtn_0e_0_usa
 """
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
+    u_name = argv[1]
+    p = argv[2]
+    db_name = argv[3]
     """lists all states from the database hbtn_0e_0_usa"""
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3])
+                         user=u_name, passwd=p,
+                         db=db_name)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id")
 
