ab8d308c067cdf812799f027291a904495af56ca
GuilleP2018 <gpereyo@gmail.com>
2023-11-24 15:43:12 -0400

    fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,6 +19,6 @@ if __name__ == "__main__":
     rows = cur.fetchall()
     for row in rows:
         if row[1][0] == 'N':
-        print(row)
+            print(row)
     cur.close()
     db.close()
